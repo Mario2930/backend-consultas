78ebaa7ef881f0c13330e9af6dbf2e937093258e
Mario S <mario2930874@gmail.com>
2026-02-27 22:33:18 -0300

    chore: criação da estrutura de pacotes em camadas

--- src/main/java/com/fiap/ec/backend_consultas/model/Especialidade.java
+++ src/main/java/com/fiap/ec/backend_consultas/model/Especialidade.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "especialidades")
+
 public class Especialidade {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
