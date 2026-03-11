17b7c7178dcbe4d6c6079b1829f727e27bff5276
Mario S <mario2930874@gmail.com>
2026-03-10 21:47:52 -0300

    feat: criação do service de Paciente

--- src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
@@ -1,5 +1,24 @@
 package com.fiap.ec.backend_consultas.service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.fiap.ec.backend_consultas.model.Paciente;
+import com.fiap.ec.backend_consultas.repository.PacienteRepository;
+@Service
 public class PacienteService {
-
+    private final PacienteRepository repository;
+    public PacienteService(PacienteRepository repository) {
+        this.repository = repository;
+    }
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
+    }
+    public List<Paciente> listar() {
+        return repository.findAll();
+    }
+    public Paciente buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paciente não encontrado"));
+    }
 }
