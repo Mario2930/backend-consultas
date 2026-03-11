b4dee13754f0ea031b9de30bbb430d3f10949b6d
Mario S <mario2930874@gmail.com>
2026-03-10 21:47:12 -0300

    feat: criação do repository de Paciente

--- src/main/java/com/fiap/ec/backend_consultas/repository/PacienteRepository.java
+++ src/main/java/com/fiap/ec/backend_consultas/repository/PacienteRepository.java
@@ -1,5 +1,6 @@
 package com.fiap.ec.backend_consultas.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class PacienteRepository {
-
+import com.fiap.ec.backend_consultas.model.Paciente;
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
