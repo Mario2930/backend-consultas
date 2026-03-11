526203f17f033362b973f9eeb0f541dff99129b1
Mario S <mario2930874@gmail.com>
2026-03-10 21:48:17 -0300

    feat: criação do controller de Paciente

--- src/main/java/com/fiap/ec/backend_consultas/controller/PacienteController.java
+++ src/main/java/com/fiap/ec/backend_consultas/controller/PacienteController.java
@@ -1,5 +1,34 @@
 package com.fiap.ec.backend_consultas.controller;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fiap.ec.backend_consultas.model.Paciente;
+import com.fiap.ec.backend_consultas.service.PacienteService;
+@RestController
+@RequestMapping("/pacientes")
+@CrossOrigin
 public class PacienteController {
-
+    private final PacienteService service;
+    public PacienteController(PacienteService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public Paciente criar(@RequestBody Paciente paciente) {
+        return service.salvar(paciente);
+    }
+    @GetMapping
+    public List<Paciente> listar() {
+        return service.listar();
+    }
+    @GetMapping("/{id}")
+    public Paciente buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
 }
